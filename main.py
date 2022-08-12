b0ce755adc345d0a370dfd30b399641811bd4240
YeghiazaryanGor <gor.yeghiazaryan.fd@gmail.com>
2022-08-12 23:11:07 +0400

    Improve code style. Improve algorithm for checking if a star belongs to fov. Add new column addition and indexation. Add current timestamp calculation.

--- main.py
+++ main.py
@@ -13,40 +13,40 @@ def csv_file_reader(filename: str) -> list:
 
 
 def filtering_dataset_by_columns(
-        csv_file_data_list: list,
+        datalist: list,
         *columns: str
 ) -> tuple[list[list[float]], dict[str, int]]:
-    stars_params = []
+    stars_list = []
     columns_position_indexes = {"id": 0}
     for col in columns:
         columns_position_indexes[col] = columns.index(col) + 1
 
-    for i in range(2, len(csv_file_data_list)):
+    for i in range(2, len(datalist)):
         try:
-            stars_params.append(
-                [float(csv_file_data_list[i][csv_file_data_list[1].index(col)])
-                 for col in columns])
-            stars_params[-1].insert(0, i - 1)
+            star = [float(datalist[i][datalist[1].index(col)]) for col in
+                    columns]
+            stars_list.append(star)
+            stars_list[-1].insert(0, i - 1)
         except ValueError:
             raise ValueError("The cell is empty or has an invalid type of "
                              "value. Please check the dataset and the row - "
                              + str(i)) from None
-    return stars_params, columns_position_indexes
+    return stars_list, columns_position_indexes
 
 
 def selecting_stars_in_fov(
         horizontal_view: float,
         vertical_view: float,
-        object_ra: float,
-        object_dec: float,
+        obj_ra: float,
+        obj_dec: float,
         stars_list: list,
         col_indexes: dict
 ) -> list:
     filtered_stars = []
-    fov_right_edge = (object_ra + horizontal_view / 2) % 360
-    fov_left_edge = (object_ra - horizontal_view / 2) % 360
-    fov_upper_edge = (object_dec + vertical_view / 2) % 360
-    fov_bottom_edge = (object_dec - vertical_view / 2) % 360
+    fov_right_edge = (obj_ra + horizontal_view / 2) % 360
+    fov_left_edge = (obj_ra - horizontal_view / 2) % 360
+    fov_upper_edge = (obj_dec + vertical_view / 2) % 360
+    fov_bottom_edge = (obj_dec - vertical_view / 2) % 360
 
     if fov_upper_edge > 90:
         raise ValueError("The upper edge of fov cannot "
@@ -56,31 +56,32 @@ def selecting_stars_in_fov(
                          "be less than -90 degrees")
 
     for i in range(len(stars_list)):
-        is_the_star_in_fovh = (fov_left_edge < 0 and (
-                360 + fov_left_edge <= stars_list[i][
-            col_indexes["ra_ep2000"]] <= 360)) or \
-                              (fov_left_edge >= 0 and (
-                                      fov_left_edge <= stars_list[i][
-                                  col_indexes["ra_ep2000"]] <= fov_right_edge))
+        ra_index = col_indexes["ra_ep2000"]
+        dec_index = col_indexes["dec_ep2000"]
+        less_than_zero = fov_left_edge < 0 and (
+                360 + fov_left_edge <= stars_list[i][ra_index] <= 360)
+        more_than_zero = fov_left_edge >= 0 and (
+                fov_left_edge <= stars_list[i][ra_index] <= fov_right_edge)
+        is_in_horizontal_fov = less_than_zero or more_than_zero
 
-        if is_the_star_in_fovh and fov_bottom_edge <= stars_list[i][
-            col_indexes["dec_ep2000"]] <= fov_upper_edge:
+        if is_in_horizontal_fov and \
+                fov_bottom_edge <= stars_list[i][dec_index] <= fov_upper_edge:
             filtered_stars.append(stars_list[i])
     return filtered_stars
 
 
 def sorting_by_column(
-        list_to_be_sorted: list,
+        to_be_sorted: list,
         column_name: str,
         col_indexes: dict
 ) -> list:
-    for i in range(len(list_to_be_sorted) - 1):
-        for j in range(len(list_to_be_sorted) - i - 1):
-            if list_to_be_sorted[j][col_indexes[column_name]] > \
-                    list_to_be_sorted[j + 1][col_indexes[column_name]]:
-                list_to_be_sorted[j], list_to_be_sorted[j + 1] = \
-                    list_to_be_sorted[j + 1], list_to_be_sorted[j]
-    return list_to_be_sorted
+    for i in range(len(to_be_sorted) - 1):
+        for j in range(len(to_be_sorted) - i - 1):
+            col = col_indexes[column_name]
+            if to_be_sorted[j][col] > to_be_sorted[j + 1][col]:
+                to_be_sorted[j], to_be_sorted[j + 1] = to_be_sorted[j + 1], \
+                                                       to_be_sorted[j]
+    return to_be_sorted
 
 
 def checking_if_there_is_stars_in_fov(
@@ -94,26 +95,35 @@ def checking_if_there_is_stars_in_fov(
         raise EmptyFov("There are no stars in this fov, please try again")
 
 
+def adding_new_column(col_name: str, col_indexes: dict):
+    col_indexes[col_name] = len(col_indexes)
+    return col_indexes
+
+
 def calculating_distance(
         stars_list: list,
         object_ra: float,
         object_dec: float,
         col_indexes: dict
-) -> tuple:
+) -> list:
     for i in range(len(stars_list)):
         temp_ra = stars_list[i][col_indexes["ra_ep2000"]]
         temp_dec = stars_list[i][col_indexes["dec_ep2000"]]
         distance = ((object_ra - temp_ra) ** 2 +
                     (object_dec - temp_dec) ** 2) ** 0.5
         stars_list[i].append(distance)
-    col_indexes["distance"] = len(col_indexes)
-    return stars_list, col_indexes
+
+    return stars_list
 
 
-def output_csv_file_creation(stars_list: list, head: list):
+def getting_todays_timestamp() -> float:
     current_time = datetime.datetime.now()
     current_timestamp = current_time.timestamp()
-    with open(str(current_timestamp) + ".csv", "w") as fl:
+    return current_timestamp
+
+
+def output_csv_file_creation(stars_list: list, head: list, timestamp: float):
+    with open(str(timestamp) + ".csv", "w") as fl:
         writer = csv.writer(fl)
         writer.writerow(head)
         writer.writerows(stars_list)
@@ -127,7 +137,7 @@ def main():
     fov_v = float(input("Write vertical field of view "))
 
     tsv_file = csv_file_reader("small_dataset.tsv")
-    stars_parameters, columns_indexes = filtering_dataset_by_columns(
+    stars_list, columns_indexes = filtering_dataset_by_columns(
         tsv_file,
         "ra_ep2000",
         "dec_ep2000",
@@ -138,19 +148,21 @@ def main():
         fov_v,
         ra,
         dec,
-        stars_parameters,
+        stars_list,
         columns_indexes
     )
     stars_in_fov = sorting_by_column(stars_in_fov, "b", columns_indexes)
     stars_in_fov = checking_if_there_is_stars_in_fov(stars_in_fov, N)
-    stars_in_fov, columns_indexes = calculating_distance(
+    columns_indexes = adding_new_column("distance", columns_indexes)
+    stars_in_fov = calculating_distance(
         stars_in_fov,
         ra,
         dec,
         columns_indexes)
     stars_in_fov = sorting_by_column(stars_in_fov, "distance", columns_indexes)
     header = ["Id", "Ra", "Dec", "Brightness", "Distance"]
-    output_csv_file_creation(stars_in_fov, header)
+    current_timestamp = getting_todays_timestamp()
+    output_csv_file_creation(stars_in_fov, header, current_timestamp)
 
 
 if __name__ == "__main__":
